7f9b5b1a9badda99cd185405c647970e22d17866
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-02 22:35:15 +0200

    Style - debugging apis

--- scripts/api.js
+++ scripts/api.js
@@ -2,27 +2,10 @@ let index = 0;
 const instance = axios.create({
   baseURL: "https://www.world-wonders-api.org/v0/wonders",
 });
-// const fetchWonders = async function () {
-//   const fetchedNames = await instance.get();
-//   let added_index = index + 10;
-//   for (
-//     let i = index;
-//     i < added_index && added_index < fetchedNames.data.length;
-//     i++
-//   ) {
-//     console.log(fetchedNames.data[i]);
-//     addWonder(fetchedNames.data[i].name, fetchedNames.data[i].links.images[0]);
-//   }
-//   index = added_index;
-// };
-
-// fetchWonders();
-
-//Another method
 
 const fetchWorldWonders = async function () {
   const fetchedData = await instance.get();
-  console.log(fetchedData);
+
   return fetchedData;
 };
 
@@ -35,14 +18,11 @@ fetchWorldWonders().then((resolved) => {
 
 function loadWonders(data) {
   let added_index = index + 10;
-
   for (let i = index; i < added_index && i < data.length; i++) {
     addWonder(data[i].name, data[i].links.images[0]);
   }
-
   index = added_index;
   if (index > data.length) {
-    console.log("Finished");
     loadMore.style.display = "none";
   }
 }
